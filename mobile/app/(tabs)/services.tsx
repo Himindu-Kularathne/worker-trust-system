7d4bbc8c750cc0aabe4d757190fd8b6a3e095f3c
Min3th <minethweerasinghe@gmail.com>
2025-12-02 07:21:51 +0530

    initialize project.

--- mobile/app/(tabs)/services.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function ServicesScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Services Page</Text>
-    </View>
-  );
-}
